fd5123a6ce25958825fe55dadd7685e7cdd61653
codeX-pratik <superpratik9861@gmail.com>
2025-02-10 17:25:59 +0530

    SpaceX Launches Tracker

---
+++ README.md
@@ -0,0 +1,6 @@
+Landing Page : 
+![alt text](<Screenshot 2025-02-10 172104.png>)
+
+
+Pagination :
+![alt text](<Screenshot 2025-02-10 172247.png>)
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LaunchTracker from "./components/LaunchTracker"
+
+function App() {
+  return (
+    <>
+      <LaunchTracker />
+    </>
+  )
+}
+
+export default App
